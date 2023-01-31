d7cedb14219e1271c8ce7827af4875607525b956
ayoub <ayoub.hamdoon@gmail.com>
2023-01-31 19:50:08 +0000

    removed old students array

--- directory.rb
+++ directory.rb
@@ -1,34 +1,4 @@
-# put all students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# print the students
-def print_header
-    puts "The students of Villain Academy"
-    puts "-----------------"
-end
-
-def print_array(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-# print the total number of students
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
+# get user input for student names
 def input_students
   puts "Please the name of the students"
   puts "To finish, please press enter twice."
@@ -45,6 +15,24 @@ def input_students
   end
   # return students array
   return students
+end
+
+# print the header
+def print_header
+    puts "The students of Villain Academy"
+    puts "-----------------"
+end
+
+# print student names
+def print_array(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+# print the total number of students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
 students = input_students
