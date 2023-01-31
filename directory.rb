2aeea04c061331e4c1543657d26f89a7366b86a7
ayoub <ayoub.hamdoon@gmail.com>
2023-01-31 19:48:09 +0000

    method to get user input for students

--- directory.rb
+++ directory.rb
@@ -29,6 +29,25 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please the name of the students"
+  puts "To finish, please press enter twice."
+  # array to store student names
+  students = []
+  # user input
+  name = gets.chomp
+  while !name.empty? do
+    # add student has to array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from user
+    name = gets.chomp
+  end
+  # return students array
+  return students
+end
+
+students = input_students
 print_header
 print_array(students)
 print_footer(students)
